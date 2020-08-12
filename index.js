54f1141b990f7ca2d4a07891e17e4bd297556be6
lance <cnmblnx@gmail.com>
2020-08-12 19:42:45 +0800

    fix yml

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ try {
   const { data: comments } = await octokit.issues.listComments({
     owner: owner,
     repo: repo,
-    issue_number: process.env.prNumber,
+    issue_number: process.env.PR_NUMBER,
   });
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
