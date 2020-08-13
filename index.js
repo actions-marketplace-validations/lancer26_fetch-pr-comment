109441ab75a98f48f49940c205ae424484a71e92
lance <cnmblnx@gmail.com>
2020-08-13 18:53:53 +0800

    fix error

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
+async function run() {
+  try {
 
-  const repository = process.env.GITHUB_REPOSITORY;
-  const [owner, repo] = repository.split("/");
+    const repository = process.env.GITHUB_REPOSITORY;
+    const [owner, repo] = repository.split("/");
 
 
-  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
-  const { data: comments } = await octokit.issues.listComments({
-    owner: owner,
-    repo: repo,
-    issue_number: process.env.PR_NUMBER,
-  });
+    const { data: comments } = await octokit.issues.listComments({
+      owner: owner,
+      repo: repo,
+      issue_number: process.env.PR_NUMBER,
+    });
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 
-  const firstCommnet = JSON.stringify(comments[0], undefined, 2);
-  console.log(`first comments ${firstCommnet}`)
-} catch (error) {
-  core.setFailed(error.message);
+    const firstCommnet = JSON.stringify(comments[0], undefined, 2);
+    console.log(`first comments ${firstCommnet}`)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
